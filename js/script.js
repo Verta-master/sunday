290362338444529f94c682309c7bd8f86af1e03b
Verta-master <vertyagina@gmail.com>
2018-12-20 20:29:37 +0600

    Update 2018-12-20T14:29:30.473Z

--- js/script.js
+++ js/script.js
@@ -7,4 +7,13 @@ $('.menu__btn').click(function() {
   $('.menu__list').slideToggle();
 })
 
+if (window.innerWidth > 766) {
+  $(window).scroll(function(){
+    if ((window.pageYOffset || document.documentElement.scrollTop ||document.body.scrollTop) >= 100) {
+      $('.header').addClass('header--scroll');
+    } else {
+      $('.header').removeClass('header--scroll');
+    };
+  });
+};
 });
